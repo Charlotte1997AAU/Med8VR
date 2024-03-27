e4b61c9b82c1949dc4930cea7a3c0dc43c59a5e1
kristian1402 <ksaren20@student.aau.dk>
2024-03-27 10:00:10 +0100

    added text and sound to buttons

---
+++ Assets/Scripts/NumberDisplay.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+
+public class NumberDisplay : MonoBehaviour
+{
+    public ButtonInteractionManager buttonManager; // Reference to the ButtonInteractionManager script
+    public TMP_Text textObject; // Reference to the TextMeshPro object to display the number
+
+    void Update()
+    {
+        // Get the specific number from ButtonInteractionManager
+        int specificNumberValue = buttonManager.GetSpecificNumber();
+
+        // Update the TextMeshPro object with the specific number
+        textObject.text = "" + specificNumberValue;
+    }
+}
